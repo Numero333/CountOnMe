680dc703e1a39939099c9c27496444758b3eabb1
xNumero3 <notnumero3@gmail.com>
2023-07-22 10:08:57 +0200

    setup calculator model

---
+++ CountOnMe/Model/CalculatorModel.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorModel.swift
+//  CountOnMe
+//
+//  Created by François-Xavier on 22/07/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+struct Calculator {
+    
+}
