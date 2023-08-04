538b67dfecaabc8b9dcceb4aa8863905e5d47bb2
xNumero3 <notnumero3@gmail.com>
2023-08-04 18:23:24 +0200

    Test Update

---
+++ CountOnMeTests/CalculatorModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorModelTests.swift
+//  CountOnMeTests
+//
+//  Created by François-Xavier on 04/08/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+final class CalculatorModelTests: XCTestCase {
+
+    let calculator = Calculator(textView: UITextView())
+    
+    
+
+}
